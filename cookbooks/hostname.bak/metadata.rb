67fd120d934a971cbc012c8caa7d2cdb3cf45a60
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2012-02-17 09:26:25 -0500

    Swapped cookbook order and removed .bak file

--- cookbooks/hostname.bak/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Maciej Pasternacki"
-maintainer_email "maciej@pasternacki.net"
-license          "MIT"
-description      "Configures hostname and FQDN"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
-
-supports "debian"
-supports "ubuntu"
