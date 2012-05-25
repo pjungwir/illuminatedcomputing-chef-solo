ce0b5b5359d0b9acbbc3778745f7e37c91699c2c
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2012-05-25 16:17:16 -0700

    Renamed to alienwords; included packages necessary for Paperclip

--- site-cookbooks/aw/recipes/default.rb
+++ site-cookbooks/aw/recipes/default.rb
@@ -1,3 +1,10 @@
+package 'imagemagick'
+package 'libxml2'
+package 'libxml2-dev'
+package 'libyaml-0-2'
+package 'libyaml-dev'
+package 'libxslt1.1'
+package 'libxslt-dev'
 
 apache_site "default" do
   enable false
@@ -7,25 +14,25 @@ apache_module 'rewrite' do
   enable true
 end
 
-directory "/var/www/angrywords" do
+directory "/var/www/alienwords" do
   owner "aw"
   group "aw"
   mode "0755"
   action :create
 end
 
-cookbook_file "#{node[:apache][:dir]}/sites-available/angrywords.conf" do
+cookbook_file "#{node[:apache][:dir]}/sites-available/alienwords.conf" do
   owner "root"
   group "root"
   mode "0644"
   action :create
 end
 
-apache_site "angrywords.conf" do
+apache_site "alienwords.conf" do
   enable true
 end
 
-directory "/var/www/angrywords-staging" do
+directory "/var/www/alienwords-staging" do
   owner "aw"
   group "aw"
   mode "0755"
@@ -33,7 +40,7 @@ directory "/var/www/angrywords-staging" do
 end
 
 dbi = data_bag_item('aw', 'staging')
-template "/var/www/angrywords-staging/passwords" do
+template "/var/www/alienwords-staging/passwords" do
   source "passwords.erb"
   owner "root"
   group "root"
@@ -43,14 +50,14 @@ template "/var/www/angrywords-staging/passwords" do
   variables :password => dbi['password']
 end
 
-cookbook_file "#{node[:apache][:dir]}/sites-available/angrywords-staging.conf" do
+cookbook_file "#{node[:apache][:dir]}/sites-available/alienwords-staging.conf" do
   owner "root"
   group "root"
   mode "0644"
   action :create
 end
 
-apache_site "angrywords-staging.conf" do
+apache_site "alienwords-staging.conf" do
   enable true
 end
 
